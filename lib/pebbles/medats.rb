eee98017e7441307faeeea22c8a87896b1231b5d
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-04-03 23:14:46 +0900

    add methods for customize outputs
    - update usage

--- lib/pebbles/medats.rb
+++ lib/pebbles/medats.rb
@@ -1,16 +1,35 @@
 require "pebbles/medats/version"
 
 module Pebbles
-  module Medats
-  private
+  class Medats
+    def initialize
+      @patterns = []
+    end
+
+    def motto(pattern = '#')
+      @patterns << pattern
+      self
+    end
+
     def medats(*args)
-      puts '####################'
-      puts '####################'
-      puts args
-      puts '####################'
-      puts '####################'
+      @patterns << '#' << '#'
+      @patterns.each {|pat| puts pat * 20 }
+      ret = puts args
+      @patterns.reverse_each {|pat| puts pat * 20 }
+      ret
+    end
+  end
+
+  module MedatsMethods
+    private
+    def motto(pattern = '#')
+      Pebbles::Medats.new.motto pattern
+    end
+
+    def medats(*args)
+      Pebbles::Medats.new.medats args
     end
   end
 end
 
-Object.send :include, Pebbles::Medats
+Object.send :include, Pebbles::MedatsMethods
